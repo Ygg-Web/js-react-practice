51410631a63ffe9bd75df65414c636c13084e1ed
Nikita <example.com>
2021-08-07 23:56:03 +0300

    first loading the created project spa

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './app-header.css';
+// import styled from 'styled-components';
+
+// const Header = styled.div`
+//     display: flex;
+//     align-items: flex-end;
+//     justify-content: space-between;
+//     h1 {
+//         font-size: 26px;
+//         color: ${props => props.colored ? 'red' : 'black'};
+//         :hover {
+//             color: blue;
+//         }
+//     }
+//     h2 {
+//         font-size: 1.2rem;
+//         color: grey;
+//     }
+// `
+
+const AppHeader = ({liked, allPosts}) => {
+    return ( 
+        <div className='app-header d-flex' >
+            <h1 > Chepik Nikita </h1> 
+            <h2>{allPosts} записей, из них понравилось {liked} </h2> 
+        </div>
+    )
+}
+
+export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app/';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// class WhoAmI extends Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             years: 26
+//         }
+//         // this.nextYear = this.nextYear.bind(this);
+//         // this.nextYear=()=>{
+//         //     this.setState(state=> ({
+//         //        years: ++state.years
+//         //     }))
+//         // }
+
+//     }
+
+//     nextYear=()=>{
+//         this.setState(state=> ({
+//            years: ++state.years
+//         }))
+//     }
+
+
+//     // nextYear() {
+//     //     // this.state.years++
+//     //     this.setState(state=> ({
+//     //         years: ++state.years
+//     //     }))
+//     // }
+
+//     render (){
+//         const {name, surname, link} = this.props;
+//         const {years} = this.state;
+//         return (
+//             <>
+//                 <button onClick ={this.nextYear}>++</button>
+//               <h1>My name is {name}, surname - {surname}, years - {years}</h1>
+//               <a href ={link}>My profile</a>
+//             </>
+//           )
+//     }
+// }
+
+
+// const All =()=>{
+//     return (
+//         <>
+//             < WhoAmI name = "John" surname="Smith" link="facebook.com" / >
+//             < WhoAmI name = "Ivan" surname="Smith" link="facebook.com" / >
+//             < WhoAmI name = "Alex" surname="Shepard" link="facebook.com" / >
+//         </>
+//     )
+// }
+
+ReactDOM.render( <App/> , document.getElementById('root'));
