21007c9612c4e4f4216cade7307c1318fbf990c6
Nikita <TsarevNF@yandex.ru>
2021-11-06 21:06:10 +0300

    added method createPost in the ApiService

--- application_blog/#src/services/api.service.js
+++ application_blog/#src/services/api.service.js
@@ -3,10 +3,19 @@ class ApiService {
         this.url = baseUrl
     }
 
-    createPost(post) {
-        fetch
+    async createPost(post) {
+        try {
+            const request = new Request(this.url + '/posts.json', {
+                method: 'post',
+                body: JSON.stringify(post)
+            })
+            const response = await fetch(request)
+            return await response.json()
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
 
 
-export const apiService = new ApiService('https://js-blog-project-ef91f-default-rtdb.europe-west1.firebasedatabase.app/')
+export const apiService = new ApiService('https://js-blog-project-ef91f-default-rtdb.europe-west1.firebasedatabase.app')
