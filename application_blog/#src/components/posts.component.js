f2a32be820434bd643a31609fab549470b087303
Nikita <TsarevNF@yandex.ru>
2021-11-08 23:05:30 +0300

    added new function renderPost in the PostComponent

--- application_blog/#src/components/posts.component.js
+++ application_blog/#src/components/posts.component.js
@@ -11,8 +11,25 @@ export class PostsComponent extends Component {
         const fbData = await apiService.fetchPosts()
         const posts = TransformService.fbObjectToArray(fbData)
         console.log(posts)
-
     }
 
 
+}
+
+function renderPost(post) {
+    return `
+        <div class="panel">
+            <div class="panel-head">
+                <p class="panel-title">Название поста</p>
+                <ul class="tags">
+                    <li class="tag tag-blue tag-rounded">Новость</li>
+                </ul>
+            </div>
+            <div class="panel-body">
+                <p class="multi-line">Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="panel-footer w-panel-footer">
+                <small>12.12.12</small>
+            </div>
+        </div>`
 }
