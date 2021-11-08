e344b5de3e5fcccc29bbd5190d45034c54cac04f
Nikita <TsarevNF@yandex.ru>
2021-11-08 23:11:15 +0300

    optimization function renderPost and onShow

--- application_blog/#src/components/posts.component.js
+++ application_blog/#src/components/posts.component.js
@@ -10,6 +10,8 @@ export class PostsComponent extends Component {
     async onShow() {
         const fbData = await apiService.fetchPosts()
         const posts = TransformService.fbObjectToArray(fbData)
+        const html = posts.map(post => renderPost(post))
+        this.elem.insertAdjacentHTML('afterbegin', html.join(' '))
         console.log(posts)
     }
 
@@ -20,16 +22,16 @@ function renderPost(post) {
     return `
         <div class="panel">
             <div class="panel-head">
-                <p class="panel-title">Название поста</p>
+                <p class="panel-title">${post.title}</p>
                 <ul class="tags">
-                    <li class="tag tag-blue tag-rounded">Новость</li>
+                    <li class="tag tag-blue tag-rounded">${post.type}</li>
                 </ul>
             </div>
             <div class="panel-body">
-                <p class="multi-line">Lorem ipsum dolor sit amet.</p>
+                <p class="multi-line">${post.fulltext}</p>
             </div>
             <div class="panel-footer w-panel-footer">
-                <small>12.12.12</small>
+                <small>${post.date}</small>
             </div>
         </div>`
 }
