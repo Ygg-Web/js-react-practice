5a9cb99a4676014e02a6c8bac2b5bf2e4af33d94
Nikita <TsarevNF@yandex.ru>
2021-11-10 22:51:09 +0300

    added method fetchPostById in the apiService

--- application_blog/#src/services/api.service.js
+++ application_blog/#src/services/api.service.js
@@ -25,6 +25,16 @@ class ApiService {
             console.error(error)
         }
     }
+    async fetchPostsById(id) {
+        try {
+            const request = new Request(`${this.url}/posts/${id}.json`, {
+                method: 'get'
+            })
+            return useRequest(request)
+        } catch (error) {
+            console.error(error)
+        }
+    }
 }
 
 async function useRequest(request) {
