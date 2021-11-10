6a84c2ca265d1c0f405832a95cefee178eba3fec
Nikita <TsarevNF@yandex.ru>
2021-11-10 23:00:56 +0300

    added function renderPost in the favoriteComponent and

--- application_blog/#src/components/favorite.component.js
+++ application_blog/#src/components/favorite.component.js
@@ -27,9 +27,37 @@
 
          const post = await apiService.fetchPostsById(postId)
          this.loader.hide()
-
+         this.elem.insertAdjacentHTML('afterbegin', renderPost(post))
      }
  }
+
+ function renderPost(post) {
+     const tag = post.type === 'news' ?
+         `<li class="tag tag-blue tag-rounded">Новость</li>` :
+         `<li class="tag tag-rounded">Заметка</li>`
+
+     const button = (JSON.parse(localStorage.getItem('favorites')) || []).includes(post.id) ?
+         `<button class="button-round button-small button-danger" data-id="${post.id}" data-post="${post.title}">Удалить</button>` :
+         `<button class="button-round button-small button-primary" data-id="${post.id}" data-post="${post.title}">Сохранить</button>`
+
+     return `
+        <div class="panel">
+            <div class="panel-head">
+                <p class="panel-title">${post.title}</p>
+                <ul class="tags">
+                    ${tag}
+                </ul>
+            </div>
+            <div class="panel-body">
+                <p class="multi-line">${post.fulltext}</p>
+            </div>
+            <div class="panel-footer w-panel-footer">
+                <small>${post.date}</small>
+                ${button}
+            </div>
+        </div>`
+ }
+
 
  function renderList(list = []) {
      if (list.length) {
