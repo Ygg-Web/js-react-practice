c39a7ad458ace588b2d85485fe413dab7ad2af40
Nikita <TsarevNF@yandex.ru>
2021-11-10 22:22:28 +0300

    added lifecycle hooks onShow and function renderList

--- application_blog/#src/components/favorite.component.js
+++ application_blog/#src/components/favorite.component.js
@@ -4,4 +4,18 @@
      constructor(id) {
          super(id)
      }
+
+     onShow() {
+         const favorites = JSON.parse(localStorage.getItem('favorites'))
+         const html = renderList(favorites)
+         this.elem.insertAdjacentHTML('afterbegin', html)
+     }
+ }
+
+ function renderList(list = []) {
+     if (list.length) {
+
+     }
+
+     return `<p class="center">Пока ничего не добавлено</p>`
  }
