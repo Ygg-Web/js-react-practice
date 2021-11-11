cec9ba4da02c30ec1da91a4739e35f07d4d8d569
Nikita <TsarevNF@yandex.ru>
2021-11-11 22:55:05 +0300

    correction of errors in the input (html) and deployment of the finished app

--- application_blog/#src/components/favorite.component.js
+++ application_blog/#src/components/favorite.component.js
@@ -37,7 +37,7 @@
  }
 
  function renderList(list = []) {
-     if (list.length) {
+     if (list && list.length) {
          return `
          <ul>
             ${list.map(i => `<li><a href='#' class='js-link'>${i}</a></li>`).join(' ')} 
