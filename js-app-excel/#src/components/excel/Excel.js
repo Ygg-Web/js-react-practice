c3620664a16f5ba60c28e3c65be94bb9ef4f158e
Nikita <TsarevNF@yandex.ru>
2021-11-16 22:30:45 +0300

    added method getRoot and render in the Excel. Render html templates of each component

--- js-app-excel/#src/components/excel/Excel.js
+++ js-app-excel/#src/components/excel/Excel.js
@@ -3,4 +3,19 @@ export class Excel {
         this.elem = document.querySelector(selector)
         this.components = options.components || []
     }
+
+    getRoot() {
+        const root = document.createElement('div')
+
+        this.components.forEach(Component => {
+            const component = new Component()
+            root.insertAdjacentHTML('beforeend', component.toHTML())
+        });
+
+        return root
+    }
+
+    render() {
+        this.elem.append(this.getRoot())
+    }
 }
--- js-app-excel/#src/components/header/Header.js
+++ js-app-excel/#src/components/header/Header.js
@@ -1,4 +1,6 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 export class Header extends ExcelComponent {
-
+    toHTML() {
+        return `<h1>Header</h1>`
+    }
 }
