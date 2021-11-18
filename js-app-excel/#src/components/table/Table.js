26c82223dd7935d23df2df9611f78dd5b47af5a8
Nikita <TsarevNF@yandex.ru>
2021-11-18 23:16:50 +0300

    created table.templates with function createtable ad private functions createcell, tocolumn, createrow, tochar

--- js-app-excel/#src/components/table/Table.js
+++ js-app-excel/#src/components/table/Table.js
@@ -1,35 +1,10 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
+import { createTable } from "./table.template";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
     toHTML() {
-        return `
-        <div class="row">
-            <div class="row-info"></div>
-            <div class="row-data">
-                <div class="column">
-                    A
-                </div>
-                <div class="column">
-                    B
-                </div>
-                <div class="column">
-                    C
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="row-info">
-                1
-            </div>
-            <div class="row-data">
-                <div class="cell selected" contenteditable>1A</div>
-                <div class="cell" contenteditable>2B</div>
-                <div class="cell" contenteditable>3C</div>
-            </div>
-        </div>
-        `
+        return createTable()
     }
 }
