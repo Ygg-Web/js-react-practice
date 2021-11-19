deaa1526b1db49dfc4dbba814bb4761a4434f9af
Nikita <TsarevNF@yandex.ru>
2021-11-19 22:25:54 +0300

    added styles for table columns and rows for resize

--- js-app-excel/#src/components/table/Table.js
+++ js-app-excel/#src/components/table/Table.js
@@ -3,8 +3,15 @@ import { createTable } from "./table.template";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
-
+    constructor(root) {
+        super(root, {
+            // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+        })
+    }
     toHTML() {
         return createTable(20)
     }
+
+
+
 }
