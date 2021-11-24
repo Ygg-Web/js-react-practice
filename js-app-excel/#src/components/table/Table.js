955828c0dc4b7c25b2c4c54d81f3db927f184a39
Nikita <TsarevNF@yandex.ru>
2021-11-24 13:21:10 +0300

    optimization resizing col and row  in the onMousedown method. using css method

--- js-app-excel/#src/components/table/Table.js
+++ js-app-excel/#src/components/table/Table.js
@@ -19,29 +19,43 @@ export class Table extends ExcelComponent {
             const parent = resizer.closest('[data-type="resizable"]')
             const coords = parent.getCoords()
             const type = resizer.data.resize
+            const sideProp = type === 'col' ? 'bottom' : 'right'
+            let value
 
-            const cells = this.root.findAll(`[data-col="${parent.data.col}"]`)
-
+            resizer.css({
+                opacity: 1,  
+                [sideProp]: '-5000px'})
 
             document.onmousemove = e => {
                 if (type === 'col') {
                     const delta = e.pageX - coords.right
-                    const value = coords.width + delta
-                        // parent.elem.style.width = value + 'px'
-                    parent.css({ width: value + 'px' })
-                    cells.forEach(el => el.style.width = value + 'px')
+                    value = coords.width + delta
+                    resizer.css({right: -delta + 'px'})
                 } else {
                     const delta = e.pageY - coords.bottom
-                    const value = coords.height + delta
-                        // parent.elem.style.height = value + 'px'
-                    parent.css({ height: value + 'px' })
+                    value = coords.height + delta
+                    resizer.css({bottom: -delta + 'px'})
 
                 }
-
             }
 
             document.onmouseup = () => {
                 document.onmousemove = null
+                document.onmouseup = null
+
+                if (type === 'col') {
+                    parent.css({width: value + 'px'})
+                    this.root.findAll(`[data-col="${parent.data.col}"]`)
+                        .forEach(el => el.style.width= value +'px')
+                } else {
+                    parent.css({height: value + 'px'})
+
+                }
+
+                resizer.css({
+                    opacity: 0,
+                    bottom: 0, 
+                    right: 0})
             }
         }
     }
