e1f0386624ede5aeaf58d08d378b50faa1427f20
Nikita <TsarevNF@yandex.ru>
2021-11-24 19:49:30 +0300

    Added method clear in class TableSelect. Made a selection of cells in  class Table

--- js-app-excel/#src/components/table/Table.js
+++ js-app-excel/#src/components/table/Table.js
@@ -1,7 +1,8 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
+import { $ } from "../../core/dom";
 import { resizeHandler } from "./table.resize";
 import { createTable } from "./table.template";
-import { shouldResize } from "./table.functions";
+import { shouldResize, isCell } from "./table.functions";
 import { TableSelection } from "./TableSelection";
 
 export class Table extends ExcelComponent {
@@ -29,6 +30,10 @@ export class Table extends ExcelComponent {
     onMousedown(event) {
         if (shouldResize(event)) {
             resizeHandler(this.root, event)
+        } else if (isCell(event)) {
+            const target = $(event.target)
+            this.selection.select(target)
         }
     }
+
 }
--- js-app-excel/#src/components/table/table.template.js
+++ js-app-excel/#src/components/table/table.template.js
@@ -12,7 +12,7 @@ const CODES = {
 function toCell(row) {
     return function(_, col) {
         return `
-        <div class="cell" data-type="resizable" contenteditable data-col="${col}" data-id="${row}:${col}"></div>
+        <div class="cell" contenteditable data-col="${col}" data-type="cell" data-id="${row}:${col}"></div>
         `
     }
 }
