2f3cd690f3bd16b8e90648511a96d89fad9a54e8
Nikita <TsarevNF@yandex.ru>
2021-12-09 23:19:43 +0300

    made toolbar creation

--- js-app-excel/#src/components/toolbar/Toolbar.js
+++ js-app-excel/#src/components/toolbar/Toolbar.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
-
+import { createToolbar } from "./toolbar.template";
+import { $ } from "../../core/dom";
 export class Toolbar extends ExcelComponent {
     static className = 'excel__toolbar'
     constructor(root, options) {
@@ -10,28 +11,14 @@ export class Toolbar extends ExcelComponent {
         })
     }
     toHTML() {
-        return `
-        <div class="button">
-            <i class="material-icons">format_align_left</i>
-        </div>
-        <div class="button">
-            <i class="material-icons">format_align_center</i>
-        </div>
-        <div class="button">
-            <i class="material-icons">format_align_right</i>
-        </div>
-        <div class="button">
-            <i class="material-icons">format_bold</i>
-        </div>
-        <div class="button">
-            <i class="material-icons">format_italic</i>
-        </div>
-        <div class="button">
-            <i class="material-icons">format_underline</i>
-        </div>`
+        return createToolbar()
     }
 
     onClick(event) {
-        console.log(event.target)
+        const target = $(event.target)
+        if (target.data.type === 'button') {
+            console.log(target.data.value)
+        }
+        // console.log(target)
     }
 }
