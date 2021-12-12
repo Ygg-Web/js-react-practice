b1fb5c4778c453acd1e680bd779696abb8b80330
Nikita <TsarevNF@yandex.ru>
2021-12-12 21:06:53 +0300

    Creating a Router

---
+++ js-app-excel/#src/core/routes/Router.js
@@ -0,0 +1,32 @@
+import { $ } from "../../core/dom";
+import { ActiveRoute } from "./ActiveRoute";
+
+export class Router {
+    constructor(selector, routes) {
+        if (!selector) {
+            throw new Error('Selector is not provided in Router')
+        }
+
+        this.placeholder = $(selector)
+        this.routes = routes
+
+        this.changePageHandler = this.changePageHandler.bind(this)
+        this.init()
+    }
+    init() {
+        window.addEventListener('hashchange', this.changePageHandler)
+        this.changePageHandler()
+    }
+
+    changePageHandler() {
+        console.log(ActiveRoute.path)
+        console.log('param', ActiveRoute.param)
+
+
+        this.placeholder.html(ActiveRoute.path)
+    }
+
+    destroy() {
+        window.removeEventListener('hashchange', this.changePageHandler)
+    }
+}
