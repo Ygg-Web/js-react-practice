882bd9fb71444b9a33f56b05200b0eee1f3ec088
Nikita <TsarevNF@yandex.ru>
2021-12-13 21:35:06 +0300

    creation and customization of the main page

--- js-app-excel/#src/pages/DashboardPage.js
+++ js-app-excel/#src/pages/DashboardPage.js
@@ -1,35 +1,23 @@
 import { $ } from "../core/dom";
 import { Page } from "../core/Page";
+import { createRecordsTable } from "./dashboard.functions";
 
 export class DashboardPage extends Page {
     getRoot() {
+        const now = Date.now().toString()
         return $.create('div', 'db').html(`
           <div class="db__header">
-              <h1>Excel Dashboard</h1>
+              <h1>Excel - Панель Управления</h1>
           </div>
           <div class="db__new">
               <div class="db__vie w">
-                  <a href="#" class="db__creat">
+                  <a href="#excel/${now}" class="db__creat">
                       Новая <br> Таблица 
                   </a>
               </div>
           </div>
           <div class="db__table db__view">
-              <div class="db__list-header">
-                  <span>Название</span>
-                  <span>Дата открытия</span>
-              </div>
-
-              <div class="db__list">
-                  <li class="db__record">
-                      <a href="#">Таблица №1</a>
-                      <strong>12.12.2021</strong>
-                  </li>
-                  <li class="db__record">
-                      <a href="#">Таблица №2</a>
-                      <strong>12.12.2021</strong>
-                  </li>
-              </div>
+            ${createRecordsTable()}
           </div>
     `)
     }
