99748589a0b143ec8833ba91ddd3365661d3aa44
Nikita <TsarevNF@yandex.ru>
2021-12-14 23:02:57 +0300

    added opening dateof the selected table

--- js-app-excel/#src/pages/dashboard.functions.js
+++ js-app-excel/#src/pages/dashboard.functions.js
@@ -6,7 +6,10 @@ function toHTML(key) {
     return `
   <li class="db__record">
     <a href="#excel/${id}">${model.title}</a>
-    <strong>12.12.2021</strong>
+    <strong>
+    ${new Date(model.openedDate).toLocaleDateString()}
+    ${new Date(model.openedDate).toLocaleTimeString()}
+    </strong>
   </li>
   `
 }
