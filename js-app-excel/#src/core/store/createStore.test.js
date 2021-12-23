cb47369f35cd650ec79c7f78adcf75757e8cec9d
Nikita <TsarevNF@yandex.ru>
2021-12-23 22:01:18 +0300

    Wrote the first test case for createStore

--- js-app-excel/#src/core/store/createStore.test.js
+++ js-app-excel/#src/core/store/createStore.test.js
@@ -1,8 +1,18 @@
 import { createStore } from './createStore'
 
-describe('TEST', () => {
-    test('test', () => {
-        const store = createStore(() => {}, {})
+const reducer = (state = 0, action) => {
+    if (action.type === 'ADD') {
+        return state + 1
+    }
+    return state
+}
+
+describe('createStore:', () => {
+    test('should return store object', () => {
+        const store = createStore(reducer)
         expect(store).toBeDefined()
+        expect(store.dispatch).toBeDefined()
+        expect(store.subscribe).toBeDefined()
+        expect(store.getState).not.toBeUndefined()
     })
 })
