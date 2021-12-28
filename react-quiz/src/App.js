1b005e2185b450a496399236948ecf504ad44860
Nikita <TsarevNF@yandex.ru>
2021-12-28 23:03:22 +0300

    creation of layout

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,10 +1,16 @@
+import React, {Component} from "react";
+import Layout from "./hoc/Layout/Layout";
 
-function App() {
-  return (
-    <div className="App"> 
-      <h1>Hello react</h1>
-    </div>
-  );
+class App extends Component {
+render(){
+    return (
+      <Layout>
+        <div style={{width: 400, border: '1px solid black', margin: '0 auto'}}>
+          <h1>Layout works</h1>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default App;
