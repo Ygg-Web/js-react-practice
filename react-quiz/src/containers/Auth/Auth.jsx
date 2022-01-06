3c42042adbf005362d27bf6a0dca341617722ff6
Nikita <TsarevNF@yandex.ru>
2022-01-06 16:08:50 +0300

    started working on validation

--- react-quiz/src/containers/Auth/Auth.jsx
+++ react-quiz/src/containers/Auth/Auth.jsx
@@ -4,6 +4,38 @@ import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 
 class Auth extends Component{
+
+  state = {
+    formControls: {
+      email:{
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный Email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+
+
+
   loginHandler = () => {
 
   }
@@ -16,19 +48,40 @@ class Auth extends Component{
     e.preventDefault()
   }
 
+  onChangeHandler =(event, controlName) => {
+    console.log(`${controlName}: `, event.target.value)
+  }
+
+  renderInputs () {
+    return Object.keys(this.state.formControls).map((controlName, index)=> {
+      const control = this.state.formControls[controlName]
+      
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
+
   render(){
     return (
       <div className={classes.Auth}>
         <div>
           <h1>Авторизация</h1>
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-              <Input label='Email'/>
-              <Input label='Пароль' errorMessage={"TEST"}/>
+              { this.renderInputs()}
 
-               
               <Button type="success" onClick={this.loginHandler}>Войти</Button>
               <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
-
           </form>
         </div>
       </div>
