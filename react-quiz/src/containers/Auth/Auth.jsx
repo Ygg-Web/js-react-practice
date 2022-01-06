893be9946d3411ee3b05df69fc9014bf2f58db2b
Nikita <TsarevNF@yandex.ru>
2022-01-06 17:00:15 +0300

    adding a page for creating tests

--- react-quiz/src/containers/Auth/Auth.jsx
+++ react-quiz/src/containers/Auth/Auth.jsx
@@ -14,6 +14,7 @@ const validateEmail = (email) => {
 class Auth extends Component{
 
   state = {
+    isFormValid: false,
     formControls: {
       email:{
         value: '',
@@ -86,8 +87,14 @@ class Auth extends Component{
 
     formControls[controlName]= control
 
+    let isFormValid  = true
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
+
     this.setState({
-      formControls
+      formControls, isFormValid
     })
   }
 
@@ -119,8 +126,8 @@ class Auth extends Component{
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
               { this.renderInputs()}
 
-              <Button type="success" onClick={this.loginHandler}>Войти</Button>
-              <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
+              <Button type="success" onClick={this.loginHandler} disabled={!this.state.isFormValid}>Войти</Button>
+              <Button type="primary" onClick={this.registerHandler} disabled={!this.state.isFormValid}>Зарегистрироваться</Button>
           </form>
         </div>
       </div>
--- react-quiz/src/containers/QuizCreator/QuizCreator.jsx
+++ react-quiz/src/containers/QuizCreator/QuizCreator.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
+import classes from './QuizCreator.module.css'
+import Button from '../../components/UI/Button/Button'
 
 class QuizCreator extends Component {
+
+  onSubmitHandler=(e)=> {
+      e.preventDefault()
+  }
+
+  addQuestionHandler=()=> {
+
+  }
+
+  createQuizHandler=()=> {
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Создание теста</h1>
+
+          <form onSubmit={this.onSubmitHandler}>
+              <input type='text'/>
+              <hr/>
+              <input type='text'/>
+              <input type='text'/>
+              <input type='text'/>
+              <input type='text'/>
+
+              <select></select>
+
+              <Button 
+                type='primary'
+                onClick={this.addQuestionHandler}
+              >
+                Добавить вопрос
+              </Button>
+
+              <Button 
+                type='success'
+                onClick={this.createQuizHandler}
+              >
+                Создать тест
+              </Button>
+          </form> 
+        </div>
       </div>
     )
   }
