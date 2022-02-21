136bf5c3ac81c5cc172dc1c713ace1bebceffb00
Nikita <TsarevNF@yandex.ru>
2022-02-21 22:27:05 +0300

    add header and main content layout

--- react-shop/src/App.js
+++ react-shop/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+      <div className="headerLogo">
+        <img width={40} height={40} src="/img/logo.png" alt=" " />
+        <div className="headerInfo">
+          <h3>React Shop</h3>
+          <p>Магазин обуви</p>
+        </div>
+      </div>
+        <ul className="headerBasket">
+          <li>
+            <img width={18} height={18} src="/img/cart.svg" alt=" " />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" alt=" " />
+          </li>
+        </ul>
       </header>
+
+      <div className="content">
+        <h1>Вся обувь</h1>
+
+       <div className="cards">
+          <div className="card">
+            <img width={133} height={112} src="/" alt=" "/>
+            <h5>Мужские боты для бегемота</h5>
+            <div className="cardBottom">
+              <div>
+                <span>Цена:</span>
+                <b>15 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/" alt=" "/>
+              </button>
+            </div>
+          </div>
+       </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
