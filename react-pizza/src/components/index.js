2c2570e79cb9dbe0bf7528ccf29b4d725fec1d69
Nikita <TsarevNF@yandex.ru>
2022-03-08 23:10:35 +0300

    creating pages: home, cart. Added router-dom

--- react-pizza/src/components/index.js
+++ react-pizza/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from "./UI/Button.jsx";
 export { default as Header } from "./Header/";
 export { default as Categories } from "./Categories/";
+export { default as SortPopup } from "./SortPopup/";
