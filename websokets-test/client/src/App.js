bad7b780378961d1c708ae37c95a4e89d05fa105
Nikita <TsarevNF@yandex.ru>
2022-04-09 22:55:58 +0300

    study of long pulling, event sourcing, websokets technologies

---
+++ websokets-test/client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { WebSock } from "./WebSock";
+
+function App() {
+  return (
+    <div>
+      <WebSock />
+    </div>
+  );
+}
+
+export default App;
---
+++ websokets-test/client/src/Longpulling.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export const Longpulling = () => {
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    subscribe();
+  }, []);
+
+  const subscribe = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5000/get-messages");
+      setMessages((prev) => [data, ...prev]);
+      await subscribe();
+    } catch (e) {
+      setTimeout(() => {
+        subscribe();
+      }, 500);
+    }
+  };
+
+  const sendMessage = async () => {
+    await axios.post("http://localhost:5000/new-messages", {
+      message: value,
+      id: Date.now(),
+    });
+  };
+
+  return (
+    <div className="center">
+      <div>
+        <div className="form">
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+        <div className="messages">
+          {messages.map((mess) => (
+            <div className="message" key={mess.id}>
+              {mess.message}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
